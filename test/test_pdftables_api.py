95914210dbf3c92b267dd3c3a1eb368dd41d918a
Peter Hynes <peter.v.hynes@googlemail.com>
2026-01-15 10:56:38 +0000

    Sensible Code to Cantabular and update copyrights

--- test/test_pdftables_api.py
+++ test/test_pdftables_api.py
@@ -1,4 +1,4 @@
-# Copyright 2016 The Sensible Code Company
+# Copyright 2026 Cantabular Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
